42c8a00d91f4e24808b180ee5b6f4d099ae0770f
ben910504 <136684274+ben910504@users.noreply.github.com>
2023-06-20 21:39:59 +0800

    update史萊姆受傷

---
+++ Assets/Scripts/DamageBasic.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class DamageBasic : MonoBehaviour
+{
+	[Header("資料")]
+	public DataBasic data;
+
+	private float hp;
+
+	private void Awake()
+	{
+		hp = data.hp;
+	}
+	public void damage(float damage)
+	{
+		hp -= damage;
+
+		print($"<color=#ffee66>{gameObject.name}血量剩下:{hp}</color>");
+
+		if (hp <= 0) Dead();
+	}
+
+	private void Dead()
+	{
+		print($"<color=#ffee66>{gameObject.name}死亡</color>");
+	}
+}
---
+++ Assets/Scripts/DamageEnemy.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class DamageEnemy : DamageBasic
+{
+	[Header("傷害值預置物")]
+	public GameObject prefabDamage;
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.name.Contains("武器"))
+		{
+			damage(50);
+			Instantiate(prefabDamage, transform.position, transform.rotation);
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		
+	}
+
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		
+	}
+}
